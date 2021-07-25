cf8e5ca3e24f7dab69d40e20546c2b4eacda6050
khuchak <83666215+khuchak@users.noreply.github.com>
2021-07-25 21:57:09 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let helpObj=require("./web-dev/help");
+let organizeObj=require("./web-dev/organize");
+let treeObj=require("./web-dev/tree");
+let arr=process.argv.slice(2);
+let inputPath=arr[1];
+if(arr[0]=="organize"){
+  organizeObj.a(inputPath);
+}
+else if(arr[0]=="tree"){
+  treeObj.b(inputPath);
+}
