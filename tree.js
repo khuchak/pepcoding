e0f4c135d1e0aca1acaf8f6f4774ef24c46f3639
khuchak <83666215+khuchak@users.noreply.github.com>
2021-07-25 21:56:32 +0530

    Create tree.js

---
+++ tree.js
@@ -0,0 +1,21 @@
+let fs=require("fs");
+let path=require("path");
+//let space="  ";
+function vie(inp){
+  let arr=fs.readdirSync(inp);
+   for(let i=0;i<arr.length;i++){
+      let checker=fs.lstatSync(path.join(inp,arr[i]));
+      if(checker.isFile()){
+          console.log(arr[i]);
+      }
+     /*  else{
+          console.log(space,arr[i]);
+          space+=" ";
+         vie(path.join(inp,arr[i]));
+         
+      } */
+  } 
+}
+module.exports={
+    b:vie
+}
